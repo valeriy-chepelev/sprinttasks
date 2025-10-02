cfe711468858803ee44f230e85bfeb0aa9f2a956
Valeriy Chepelev <valeriychepelev@gmail.com>
2025-10-02 12:51:01 +0300

    retag complete

--- source/retag.py
+++ source/retag.py
@@ -1,5 +1,9 @@
+import time
+
 from yandex_tracker_client import TrackerClient
 import configparser
+import argparse
+from alive_progress import alive_bar
 
 
 def read_config(filename):
@@ -10,21 +14,52 @@ def read_config(filename):
     return config['DEFAULT']
 
 
-def tag_issues(client, queue, tag, assignee=''):
-    a = '' if assignee == '' else ''  # TODO: list to csv, and AND
-    request = f'Queue: "{queue}" AND Tags:!"{tag}" {a} "Sort by":Key ASC'
+def get_issues(client, queue: str, tag: str, persons: list) -> list:
+    request = f'Queue: "{queue}" AND Tags:!"{tag}" "Sort by":Key ASC'
     issues = client.issues.find(query=request)
-    print(f'{len(issues)} issue(s) to be tagged with "{tag}".')
+    if len(persons) > 0:
+        with alive_bar(len(issues), title='Filter', theme='classic') as bar:
+            res = [issue for issue in issues if bar() != 'foo'
+                   and issue.assignee is not None
+                   and next((p for p in persons if p.lower() in issue.assignee.display.lower()), False)]
+        return res
+    else:
+        return issues
+
+
+def tag_issues(issues, tag):
+    with alive_bar(len(issues), title='Tagging', theme='classic') as bar:
+        for issue in issues:
+            issue.update(tags={'add': tag})
+            time.sleep(1)
+            bar()
 
 
 def main():
+    # CLI parser
+    parser = argparse.ArgumentParser(description='Task tagger by VCh.')
+    parser.add_argument('queue', help='queue name')
+    parser.add_argument('tag', help='tag value')
+    parser.add_argument('assignee', nargs='*', default=[],
+                        help='assignees')
+    args = parser.parse_args()
+
+    # connection
     cfg = read_config('sprint.ini')
     client = TrackerClient(cfg['token'], cfg['org'])
     if client.myself is None:
         raise Exception('Unable to connect Yandex Tracker.')
     print('Connected to Yandex Tracker.')
-    tag_issues(client, 'MTFW', 'fw')
-    tag_issues(client, 'MTHW', 'hw')
+
+    # tagging
+    issues = get_issues(client, args.queue, args.tag, args.assignee)
+    print(f'{len(issues)} issue(s) in {args.queue} to be tagged with "{args.tag}".')
+    if len(args.assignee) > 0:
+        persons = {p.assignee.display for p in issues}
+        print(f'Assignees found: {", ".join(persons)}.')
+    confirm = input('Enter Y to proceed:')
+    assert confirm == 'Y'
+    tag_issues(issues, args.tag)
 
 
 if __name__ == '__main__':
@@ -32,4 +67,3 @@ if __name__ == '__main__':
         main()
     except Exception as e:
         print('Execution error:', e)
-        input('Press any key to close...')
