5a295e80340bbf93091f903ca208ac01e57005a7
Valeriy Chepelev <valeriychepelev@gmail.com>
2026-03-11 14:25:26 +0300

    Sort projects by rating

--- source/sprint.py
+++ source/sprint.py
@@ -4,6 +4,28 @@ import argparse
 from prettytable import PrettyTable, TableStyle
 import pyperclip
 
+import requests
+
+
+def projects(token, org):
+    """Return list of project names, up to 1000 projects
+    token, org is OAuth credentials"""
+    session = requests.Session()
+    headers = {"Host": "api.tracker.yandex.net",
+               "Authorization": f"OAuth {token}"}
+    if len(org) < 15:
+        headers.update({"X-Org-ID": org})
+    else:
+        headers.update({"X-Cloud-Org-ID": org})
+    request = 'https://api.tracker.yandex.net/v2/entities/project/_search'
+
+    params = {'fields': 'summary,entityRank', 'perPage': '1000'}
+    response = session.post(url=request, headers=headers, params=params, data={})
+    response.raise_for_status()
+    data = response.json()
+
+    return {project['fields']['summary']: project['fields']['entityRank'] for project in data['values']}
+
 
 def read_config(filename):
     config = configparser.ConfigParser()
@@ -13,21 +35,26 @@ def read_config(filename):
     return config['DEFAULT']
 
 
-def dupe_sprint(client, sprint_name):
+def dupe_sprint(client, sprint_name, ranks):
     request = f'Sprint: "{sprint_name}" "Sort by":Project ASC'
     issues = client.issues.find(query=request)
     print(sprint_name)
     table = PrettyTable()
-    table.field_names = ['Project', 'Issue', 'Summary', 'Assignee', 'Report']
+    table.field_names = ['Project', 'Rank', 'Issue', 'Summary', 'Assignee', 'Report']
     table.add_rows([[issue.project.name if issue.project is not None else '-',
+                     0 if issue.project.name not in ranks or
+                            ranks[issue.project.name] is None
+                     else ranks[issue.project.name],
                      issue.key,
                      issue.summary,
                      issue.assignee.display,
                      '']
                     for issue in issues])
     table.align = 'l'
+    table.sortby = 'Rank'
+    table.reversesort = True
     table.set_style(TableStyle.MARKDOWN)
-    pyperclip.copy(f'# {sprint_name}\n' + table.get_formatted_string())
+    pyperclip.copy(f'# {sprint_name}\n' + table.get_formatted_string(fields=['Project', 'Issue', 'Summary', 'Assignee', 'Report']))
     table.set_style(TableStyle.DEFAULT)
     print(table)
 
@@ -44,7 +71,10 @@ def main():
     parser.add_argument('sprint',
                         help='sprint name')
     args = parser.parse_args()
-    dupe_sprint(client, args.sprint)
+
+    ranks = projects(creds['token'], creds['org'])
+
+    dupe_sprint(client, args.sprint, ranks)
     print('Markdown copied to system clipboard.')
 
 
